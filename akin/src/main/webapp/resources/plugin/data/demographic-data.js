d89b20f4e6e29ffea917260736b1fadb34e40c19
lupitarios <mariag.riosm@gmail.com>
2017-07-14 12:01:06 -0500

    [MGRM] Se cambio la pantalla de Seguidores para que se muestren dentro de Tabs.

--- akin/src/main/webapp/resources/plugin/data/demographic-data.js
+++ akin/src/main/webapp/resources/plugin/data/demographic-data.js
@@ -99,6 +99,45 @@ var politicalPreferencesBar = Morris.Line({
 	
 });
 
+var followersTotal =	Morris.Bar({
+    element: 'followers-bar-chart',
+    data: [{
+        y: 'Total - 715700',
+        a: 10000,
+        b: -9000
+    	}
+    ],
+    xkey: 'y',
+    ykeys: ['a', 'b'],
+    labels: ['Nuevos', 'Dejaron de seguir'],
+    hideHover: 'auto',
+    resize: true
+});
+
+var followersTopten =	Morris.Bar({
+    element: 'followers-top-ten',
+    data: [{
+        y: 'Seguidores ',
+        a: 100,
+        b: 90,
+        c: 100,
+        d: 90,
+        e: 100,
+        f: 90,
+        g: 80,
+        h: 90,
+        i: 90,
+        j: 90
+        
+    	}
+    ],
+    xkey: 'y',
+    ykeys: ['a','b','c','d','e','f','g','h','i','j'],
+    labels: ['Seguidor 1', 'Seguidor 2', 'Seguidor 3', 'Seguidor 4', 'Seguidor 5','Seguidor 6', 'Seguidor 7', 'Seguidor 8', 'Seguidor 9', 'Seguidor 10'],
+    hideHover: 'auto',
+    resize: true
+});
+
 
 $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
 	  var target = $(e.target).attr("href") // activated tab
@@ -123,6 +162,15 @@ $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
 	    case "#political-tab":
 	    	politicalPreferencesBar.redraw();
 	      $(window).trigger('resize');
+	      break;
+	    case "#followersTotal-tab":
+	    	followersTotal.redraw();
+	      $(window).trigger('resize');
 	      break; 
+	    case "#followersTopten-tab":
+	    	followersTopten.redraw();
+	      $(window).trigger('resize');
+	      break;
+	  
 	  }
 	});
